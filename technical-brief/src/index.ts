ec1463541709c1daff72043a94be452ce895a2c0
Espen Zaal <espenbz@gmail.com>
2019-03-17 11:28:40 +0100

    Specifying multipart parser and enabling auto migrate of schema.

--- technical-brief/src/index.ts
+++ technical-brief/src/index.ts
@@ -1,13 +1,15 @@
 import {TechnicalBriefApplication} from './application';
 import {ApplicationConfig} from '@loopback/core';
+import MultipartFormDataBodyParser from './controllers/MultipartFormDataBodyParser';
 
 export {TechnicalBriefApplication};
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new TechnicalBriefApplication(options);
+  app.bodyParser(MultipartFormDataBodyParser);
   await app.boot();
+  await app.migrateSchema(); //Create schema if not exists.
   await app.start();
-
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
   console.log(`Try ${url}/ping`);
