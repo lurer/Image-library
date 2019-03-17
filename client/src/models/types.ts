7244f2b54d678077aed2d13cb0b68e43b17a711e
Espen Zaal <espenbz@gmail.com>
2019-03-17 18:14:54 +0100

    types.ts to gather models and interface declarations

---
+++ client/src/models/types.ts
@@ -0,0 +1,25 @@
+
+
+export interface AppContextInterface {
+    serverEnv: AppContextServer
+}
+
+export interface AppContextServer {
+    server: string;
+    apiEndpoint: string;
+    port: number;
+}
+
+export interface BaseModel {
+
+}
+
+export interface FileObject extends BaseModel {
+    fieldname: string;
+    orgiginalname: string;
+    encoding: string;
+    mimetype: string;
+    buffer: Buffer;
+    size: number;
+    id: number;
+}
