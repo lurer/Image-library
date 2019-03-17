64ed08cf5c78a67b43811cab80ff2cdfda684ad6
Espen Zaal <espenbz@gmail.com>
2019-03-17 18:16:49 +0100

    Basic App using react lazy, context and suspense features.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Suspense, lazy, ComponentType } from 'react';
+import LoadingPage from './components/Loading/LoadingPage';
+import { AppContextProvider } from './contexts/AppContext';
+import { AppContextInterface } from './models/types';
+
+
+
+const appContext: AppContextInterface = {
+  serverEnv : {
+    server: "localhost",
+    apiEndpoint: "/",
+    port: 3000
+  }
+}
+
+
+const FileList = (
+  lazy(() : Promise<{default:ComponentType}> => (
+    import("./components/File/FileList")
+  ))
+)
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <AppContextProvider value={appContext}>
+            <Suspense fallback={LoadingPage}>
+              <FileList />
+            </Suspense>
+          </AppContextProvider>
         </header>
       </div>
     );
