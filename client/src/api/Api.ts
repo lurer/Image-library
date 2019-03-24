f59fb81910a6f9c1b6708bd6563f27cdfb453efe
Espen Zaal <espenbz@gmail.com>
2019-03-24 12:50:06 +0100

    Implemented a simple getAll. No error handling yet

--- client/src/api/Api.ts
+++ client/src/api/Api.ts
@@ -1,15 +1,18 @@
 import { BaseModel, AppContextServer } from "../models/types";
 import { GenericApi } from './GenericApi';
 import axsios from 'axios';
+import Axios from "axios";
 
 export class Api<T extends BaseModel> implements GenericApi<T> {
     
     protected env : AppContextServer
     protected endpoint: string;
+    protected uri: string;
 
     constructor(serverEnv: AppContextServer, endpoint: string){
         this.env = serverEnv;
-        this.endpoint = endpoint
+        this.endpoint = endpoint;
+        this.uri = `${this.env.server}:${this.env.port}${this.env.apiEndpoint}${endpoint}`
     }
 
     get = async (id: number) : Promise<T> => {
@@ -17,7 +20,8 @@ export class Api<T extends BaseModel> implements GenericApi<T> {
     }
     
     getAll = async () : Promise<T[]> => {
-        return {} as any;
+        const result = await Axios.get<T[]>(this.uri);
+        return result.data;
     }
     create = async (object: T) : Promise<T> => {
         return {} as any;
