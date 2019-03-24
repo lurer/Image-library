b15ffff88255bde2a7b38b1a328071096da1de8e
Espen Zaal <espenbz@gmail.com>
2019-03-24 12:49:30 +0100

    Post method for File/Blob

--- client/src/api/FileApi.ts
+++ client/src/api/FileApi.ts
@@ -1,10 +1,20 @@
 import { BaseModel } from "../models/types";
 import { Api } from './Api';
+import Axios from "axios";
 
-export class FileApi<T extends BaseModel> extends Api<T> {
+export class FileApi<T extends BaseModel|Blob> extends Api<T> {
     
     uploadFile = async (object: T) : Promise<T> => {
-        return {} as any;
+        const formData = new FormData();
+        formData.append("file", object as Blob);
+        const config = {
+            headers:{
+                "content-type": "multipart/form-data"
+            }
+        }
+        const response = await Axios.post<T>(this.uri, formData, config);
+        return response.data
+        
     };
     
 }
