77576bc0c4e998c288b967a3dd2fa546603d005e
Espen Zaal <espenbz@gmail.com>
2019-04-07 02:00:07 +0200

    AppContext defines an "alertMessage" method. Children components can send alert messages through AppContext for info messages and errors.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Suspense, lazy, ComponentType, createContext } from 'react';
+import React, { Component, Suspense, lazy, ComponentType, createContext, useState } from 'react';
 import LoadingPage from './components/Loading/LoadingPage';
 import { AppContextProvider } from './contexts/AppContext';
 import { AppContextInterface } from './models/types';
@@ -7,14 +7,12 @@ import * as types from './models/types';
 import styles from './app.module.scss';
 import SortMethods from './components/File/SortMehtods';
 import FileTopWrapper from './components/File/FileTopWrapper';
+import StatusMessage from './components/StatusMessage/StatusMessage';
 
-const appContext: AppContextInterface = {
-  serverEnv: {
-    server: "http://localhost",
-    apiEndpoint: "/",
-    port: 3000
-  }
-}
+
+
+
+
 
 const FileList = (
   lazy((): Promise<{ default: ComponentType }> => (
@@ -28,33 +26,48 @@ const NewFile = (
   ))
 )
 
-class App extends Component {
+const App = () => {
 
-  render() {
-    return (
-      <div className={styles.app}>
-        <header className={styles.appHeader}>
+  const [messages, setMessage] = useState<Array<types.StatusMessage>>([])
 
-        </header>
-        <div className={styles.content}>
-          <AppContextProvider value={appContext}>
-            <Suspense fallback={<LoadingPage />}>
-              <FileWrapper>
-                <FileTopWrapper>
-                    <NewFile />
-                    <SortMethods />
-                </FileTopWrapper>
-                
-                <FileList />
-
-              </FileWrapper>
-            </Suspense>
-          </AppContextProvider>
-        </div>
-
-      </div>
-    );
+  const fireAlert = (message: types.StatusMessage) => {
+    setMessage([...messages,message])
+    setTimeout(() => {
+      setMessage(messages.filter((m: types.StatusMessage) => m.title != message.message))
+    }, message.type == "info" ? 1000 : 2000)
   }
+
+
+  const appContext: AppContextInterface = {
+    serverEnv: {
+      server: "http://localhost",
+      apiEndpoint: "/",
+      port: 3000
+    },
+    alertMessage: fireAlert
+  }
+
+  return (
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+          {messages.map((message, index) => <StatusMessage {...message} key={index}/>)}
+      </header>
+      <div className={styles.content}>
+        <AppContextProvider value={appContext}>
+          <Suspense fallback={<LoadingPage />}>
+            <FileWrapper>
+              <FileTopWrapper>
+                <NewFile />
+                <SortMethods />
+              </FileTopWrapper>
+              <FileList />
+            </FileWrapper>
+          </Suspense>
+        </AppContextProvider>
+      </div>
+
+    </div>
+  );
 }
 
 export default App;
